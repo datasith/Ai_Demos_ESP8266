e0b7b671463be2d9d349b01164e2b45fca49dfd2
makerbro <makerbro@acrobotic.com>
2016-04-10 02:18:17 -0700

    updating

--- i2c_oled/README.md
+++ i2c_oled/README.md
@@ -1,23 +1,27 @@
-# Ai_Demos_ESP8266
+# iot_weather_station
 
-Example code for using the ESP8266 Development Board with various electronic and
-mechanical components.
+Example code for using the ESP8266 Development Board and an OLED display for
+visualizing different things on its 128×64 pixel screen.
 
 ## Description
 
 For more details, check out the tutorial page at:
 
-    * http://learn.acrobotic.com/tutorials/post/esp8266-iot-weather-station
-    * http://learn.acrobotic.com/tutorials/post/esp8266-oled-display-using-i2c
+   * http://learn.acrobotic.com/tutorials/post/esp8266-oled-display-using-i2c
 
 Developed by S.Parada for ACROBOTIC Industries.  Please consider buying 
 products from us to help fund future Open-Source projects like this! We'll
 always put our best effort in every project, and release all our design 
-files and code for you to use. 
+files and code for you to use.
 
 Visit our online shop, which is located at:
 
    * https://acrobotic.com/
+
+## Installation
+
+Please follow the instructions outlined in the tutorial linked above.  The steps
+describe how to install the library needed for the programs to run properly.
 
 ## License
 
---
+++ i2c_oled/i2c_oled.ino
@@ -0,0 +1,94 @@
+#include <Wire.h>
+#include "util.h"
+
+void setup()
+{
+  Wire.begin();	
+  displayInit();                // initialze OLED display
+  displayClear();               // clear the display
+  displayDraw(LOGO,1024);       // 1024 = 128 Pixels * 64 Pixels / 8
+  setTextXY(0,0);               // Set the cursor position to 0th page (row), 0th column
+  displayString("   ACROBOTIC   ");
+}
+
+void loop()
+{
+}
+
+void sendData(unsigned char data)
+{
+  Wire.beginTransmission(I2C_ADDRESS);  // begin I2C transmission
+  Wire.write(CMD_MODE_DATA);            // set mode: data
+  Wire.write(data);
+  Wire.endTransmission();               // stop I2C transmission
+}
+
+void sendCommand(unsigned char command)
+{
+  Wire.beginTransmission(I2C_ADDRESS);  // begin I2C communication
+  Wire.write(CMD_MODE_COMMAND);         // set mode: command
+  Wire.write(command);
+  Wire.endTransmission();               // End I2C communication
+}
+
+void displayInit()
+{
+  sendCommand(CMD_DISPLAY_OFF);         // display off
+  delay(5);
+  sendCommand(CMD_DISPLAY_ON);          // display on
+  delay(5);
+  sendCommand(CMD_DISPLAY_NORMAL);  // display can be either normal on inverse
+}
+
+void setTextXY(unsigned char row, unsigned char col)
+{
+    sendCommand(0xB0 + row);                //set page address
+    sendCommand(0x00 + (8*col & 0x0F));     //set column lower address
+    sendCommand(0x10 + ((8*col>>4)&0x0F));  //set column higher address
+}
+
+void displayString(const char *str)
+{
+    unsigned char i=0, j=0, c=0;
+    while(str[i])
+    {
+      c = str[i++];
+      if(c < 32 || c > 127) //Ignore non-printable ASCII characters. This can be modified for multilingual font.
+      {
+        c=' '; //Space
+      }
+      for(j=0;j<8;j++)
+      {
+         //read bytes from code memory
+         sendData(pgm_read_byte(&BasicFont[c-32][j])); //font array starts at 0, ASCII starts at 32. Hence the translation
+      }
+    }
+}
+
+void displayClear()
+{
+  unsigned char i=0, j=0;
+  sendCommand(CMD_DISPLAY_OFF);     //display off
+  for(j=0;j<8;j++)
+  {        
+    setTextXY(j,0);                                                                            
+    {   
+      for(i=0;i<16;i++)  //clear all columns
+      {   
+        displayString((const char*)" ");         
+      }   
+    }   
+  }
+  sendCommand(CMD_DISPLAY_ON);     //display on
+  setTextXY(0,0);   
+}
+
+void displayDraw(const unsigned char *bitmaparray,int bytes)
+{
+  sendCommand(0x20);                      //set addressing mode
+  sendCommand(0x00);                      //set horizontal addressing mode
+  for(int i=0;i<bytes;i++)
+  {
+      sendData(pgm_read_byte(&bitmaparray[i]));
+  }
+}
